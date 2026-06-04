f69b069e529c336d2a87f6a6a72176beb1d5c42e
Bruno Trestini <bruno.trestini@gmail.com>
2026-06-04 00:02:16 -0300

    criacao pages dashboard

---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,90 @@
+// import { Component, OnInit } from '@angular/core';
+
+// import { FormsModule } from '@angular/forms';
+
+// import Swal from 'sweetalert2';
+
+// import { ClienteService } from '../../services/cliente.service';
+
+// import { Cliente } from '../../models/cliente';
+
+// @Component({
+//   selector: 'app-clientes',
+
+//   standalone: true,
+
+//   imports: [FormsModule],
+
+//   templateUrl: './dashboard.html',
+
+//   styleUrl: './dashboard.css',
+// })
+// export class ClientesComponent implements OnInit {
+//   clientes: Cliente[] = [];
+
+//   mostrarTabela = false;
+
+//   novoCliente: Cliente = {
+//     nome: '',
+//     telefone: '',
+//     email: '',
+//   };
+
+//   constructor(private clienteService: ClienteService) {}
+
+//   ngOnInit(): void {
+//     this.carregarClientes();
+//   }
+
+//   toggleTabela(): void {
+//     this.mostrarTabela = !this.mostrarTabela;
+//   }
+
+//   carregarClientes(): void {
+//     this.clienteService.listarClientes().subscribe({
+//       next: (dados) => {
+//         this.clientes = dados;
+//       },
+
+//       error: (erro) => {
+//         console.log(erro);
+//       },
+//     });
+//   }
+
+//   cadastrarCliente(): void {
+//     this.clienteService.criarCliente(this.novoCliente).subscribe({
+//       next: () => {
+//         Swal.fire({
+//           icon: 'success',
+
+//           title: 'Sucesso',
+
+//           text: 'Cliente cadastrado com sucesso!',
+
+//           confirmButtonColor: '#3b82f6',
+//         });
+
+//         this.carregarClientes();
+
+//         this.novoCliente = {
+//           nome: '',
+//           telefone: '',
+//           email: '',
+//         };
+//       },
+
+//       error: (erro) => {
+//         Swal.fire({
+//           icon: 'error',
+
+//           title: 'Erro',
+
+//           text: erro.error?.detail || 'Erro ao cadastrar cliente.',
+
+//           confirmButtonColor: '#1e3a8a',
+//         });
+//       },
+//     });
+//   }
+// }
